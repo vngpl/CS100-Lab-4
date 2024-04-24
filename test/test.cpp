8f7b368c354dd5f0add9af323b6efa51ffc5db1b
Jingbo Su <sujingbo0217@gmail.com>
2024-04-24 13:46:45 -0700

    Award tests commit

--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,46 @@
-#include "gtest/gtest.h"
-#include "../include/Triangle.h"
-#include "../include/Awards.h"
 #include <vector>
-using shapes::Triangle;
 
+// #include "../include/Triangle.h"
+#include "gtest/gtest.h"
+// using shapes::Triangle;
 
-TEST(TriangleTests, testPerimeter) {
-    Triangle *aTriangle = new Triangle(3,3,3);
-    EXPECT_EQ (aTriangle->getPerimeter(),9);
-}
+#include "../include/Awards.h"
+using awards::AwardCeremonyActions;
+using awards::RankList;
 
 class MockRankList : public RankList {
-public:
-    std::string getNext() {
-        return "AAA";
-    }
+ public:
+  std::string getNext() { return "AAA"; }
 };
+
+// Test MockRankList
+// TEST(AwardsTesting, GetNextTest) {
+//   MockRankList* ranklist = new MockRankList();
+//   EXPECT_EQ(ranklist->getNext(), "AAA");
+// }
+
+class MockAwardCeremonyActions : public AwardCeremonyActions {
+ public:
+  void playAnthem() {
+    std::cout << "Playing the anthem" << std::endl;
+  }
+  void turnOffTheLightsAndGoHome() {
+    std::cout << "Turning off the lights and going home" << std::endl;
+  }
+  void awardBronze(std::string recipient) {
+    std::cout << "Awarding bronze medal to " << recipient << std::endl;
+  }
+  void awardSilver(std::string recipient) {
+    std::cout << "Awarding silver medal to " << recipient << std::endl;
+  }
+  void awardGold(std::string recipient) {
+    std::cout << "Awarding gold medal to " << recipient << std::endl;
+  }
+};
+
+// Test MockAwardCeremonyActions
+TEST(AwardsTests, performAwardCeremonyTest) {
+  MockRankList ranklist;
+  MockAwardCeremonyActions actions;
+  awards::performAwardCeremony(ranklist, actions);
+}
