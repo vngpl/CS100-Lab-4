82a962948a88c1eed8ccbb7136622ef47eaa251b
Jingbo Su <sujingbo0217@gmail.com>
2024-04-24 11:44:04 -0700

    initial commit

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,23 @@
-#include "gtest/gtest.h"
 #include "../include/Triangle.h"
+#include "gtest/gtest.h"
 using shapes::Triangle;
 
+// TEST(TriangleTests, testPerimeter) {
+//   Triangle *aTriangle = new Triangle(3, 3, 3);
+//   EXPECT_EQ(aTriangle->getPerimeter(), 9);
+// }
 
 TEST(TriangleTests, testPerimeter) {
-    Triangle *aTriangle = new Triangle(3,3,3);
-    EXPECT_EQ (aTriangle->getPerimeter(),9);
+  Triangle *aTriangle = new Triangle(3, 2, 3);
+  EXPECT_EQ(aTriangle->getPerimeter(), 8);
+}
+
+TEST(TriangleTests, testArea) {
+  Triangle *aTriangle = new Triangle(5, 4, 3);
+  EXPECT_EQ(aTriangle->getArea(), 6);
+}
+
+TEST(TriangleTests, testKind) {
+  Triangle *aTriangle = new Triangle(3, 3, 3);
+  EXPECT_EQ(aTriangle->getKind(), Triangle::Kind::EQUILATERAL);
 }
