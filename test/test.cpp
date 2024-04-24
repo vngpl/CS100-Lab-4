54c47f20096fe601b185ff4efe35c395092cd1e3
vngpl <v.ngpl1911@gmail.com>
2024-04-24 12:56:54 -0700

    added TriangleTests

--- test/test.cpp
+++ test/test.cpp
@@ -7,6 +7,7 @@ using shapes::Triangle;
 //   EXPECT_EQ(aTriangle->getPerimeter(), 9);
 // }
 
+// 3 tests to check perimeter, area, and kind
 TEST(TriangleTests, testPerimeter) {
   Triangle *aTriangle = new Triangle(3, 2, 3);
   EXPECT_EQ(aTriangle->getPerimeter(), 8);
@@ -14,10 +15,30 @@ TEST(TriangleTests, testPerimeter) {
 
 TEST(TriangleTests, testArea) {
   Triangle *aTriangle = new Triangle(5, 4, 3);
-  EXPECT_EQ(aTriangle->getArea(), 6);
+  EXPECT_DOUBLE_EQ(aTriangle->getArea(), 6.0);
 }
 
-TEST(TriangleTests, testKind) {
+TEST(TriangleTests, testKindEquilateral) {
   Triangle *aTriangle = new Triangle(3, 3, 3);
   EXPECT_EQ(aTriangle->getKind(), Triangle::Kind::EQUILATERAL);
+}
+
+// 1 test to check death
+TEST(TriangleTests, testDeath) {
+  EXPECT_DEATH({Triangle tri(1, 1, 2);}, "invalid triangle");
+}
+
+// 3 additional tests
+TEST(TriangleTests, testNoThrow) {
+  EXPECT_NO_THROW({Triangle tri(5, 4, 3);});
+}
+
+TEST(TriangleTests, testKindIsosceles) {
+  Triangle *aTriangle = new Triangle(3, 3, 2);
+  EXPECT_EQ(aTriangle->getKind(), Triangle::Kind::ISOSCELES);
+}
+
+TEST(TriangleTests, testKindScalene) {
+  Triangle *aTriangle = new Triangle(5, 4, 3);
+  EXPECT_EQ(aTriangle->getKind(), Triangle::Kind::SCALENE);
 }
